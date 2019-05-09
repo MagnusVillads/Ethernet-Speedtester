371edbf28dc39d39c529c93dcc914c51b134867f
MagnusVillads <50359943+MagnusVillads@users.noreply.github.com>
2019-05-09 10:05:53 +0200

    Add files via upload

--- speedtest.sh
+++ speedtest.sh
@@ -1,11 +1,16 @@
 echo Angiv Filnavn
 read filnavn
 echo
-echo Læn Dig Tilbage Mens Jeg Klare Arbejdet
-echo
 python -u speedtest.py | tee $filnavn
 echo
 echo Din Test Er Færdig
 echo
 echo Credit: Magnus Villads
+echo
+echo
+echo
+echo
+echo
+echo
+echo
 
